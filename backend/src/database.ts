0ebe635a2245cf8a53bc29c6d1634c9b2f972227
Jeni1336 <jeniffer1336@gmail.com>
2024-07-28 20:24:09 -0300

    atualizações no front e back

---
+++ backend/src/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  models: [__dirname + '/../models'], 
+});
+
+export default sequelize;
