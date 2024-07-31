9b2cc9659d65f01ef2d1f1be88dcebec2e278786
Jeni1336 <jeniffer1336@gmail.com>
2024-07-30 22:31:17 -0300

    resolvendo erros

--- backend/dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('finance_app', 'root', 'your_password', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
-});
-exports.default = sequelize;
